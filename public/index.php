fec9a3ce4c4d9381ef5bd1c98a65a0557f3b539b
marioyunis <espy88@gmail.com>
2023-04-22 15:34:27 -0500

    Estructura nueva

--- public/index.php
+++ public/index.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+require_once '../vendor/autoload.php';
+
+session_start();
+
+use App\Controllers\HomeController;
+use App\Controllers\AuthController;
+
+$requestUri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+
+switch ($requestUri) {
+    case '/':
+        $controller = new HomeController();
+        echo $controller->index();
+        break;
+
+    case '/login':
+        $controller = new AuthController();
+        echo $controller->login();
+        break;
+
+    case '/logout':
+        $controller = new AuthController();
+        echo $controller->logout();
+        break;
+
+    default:
+        header("HTTP/1.0 404 Not Found");
+        echo "Not Found";
+        break;
+}
